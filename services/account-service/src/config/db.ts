7da3cb41f5c4f6badb15a0ba7c432e09896ee149
Dejan Vitomirov <dejan.vitomirov@gmail.com>
2025-12-22 16:28:57 +0100

    fix: replace schema synchronization with typeorm migrations

--- services/account-service/src/config/db.ts
+++ services/account-service/src/config/db.ts
@@ -1,20 +1,34 @@
 import { DataSource } from "typeorm";
 import { User } from "../entities/User";
+import * as path from "path";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   schema: "public",
-  synchronize: true,
+  synchronize: false,
   logging: true,
   entities: [User],
+  migrations: [path.join(__dirname, "..", "migrations", "*.ts")],
 });
 
 export async function initializeDB() {
-  await AppDataSource.initialize();
-  console.log("account DB connected");
+  let retries = 5;
+  while (retries) {
+    try {
+      await AppDataSource.initialize();
+      console.log("Account DB connected");
+      
+      await AppDataSource.runMigrations();
+      console.log("Account migrations executed");
+      break;
+    } catch (err) {
+      console.error(`Error connecting to Account DB: ${err}. Retries left: ${retries}`);
+      retries -= 1;
+      await new Promise(res => setTimeout(res, 5000));
+    }
+  }
 }
